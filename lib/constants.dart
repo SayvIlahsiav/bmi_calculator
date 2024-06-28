5daf0702e6001bc76b9684973694d0787d2cec28
Vaishali Vyas <sayvilahsiav@outlook.com>
2024-06-28 19:17:28 +0530

    Added constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kColorLightBlue = Color(0xffB4DAF5);
+const kColorLightGreen = Color(0xffBBEEBA);
+const kColorLightYellow = Color(0xffFFECBA);
+const kColorDarkText = Color(0xff112200);
+const kColorDarkBrown = Color(0xff914C3C);
+const kColorDarkGreen = Color(0xff467302);
+const kColorDarkYellow = Color(0xffD2DD13);
+const kColorLightText = Color(0xffEDF8E1);
