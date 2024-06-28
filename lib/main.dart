b89e70069af4bd5572f3a282bb739057b7887b64
Vaishali Vyas <sayvilahsiav@outlook.com>
2024-06-28 19:18:06 +0530

    Updated Scaffold background

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/constants.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(BMICalculator());
@@ -21,6 +22,21 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: <Color>[
+              kColorLightBlue,
+              kColorLightGreen,
+              kColorLightYellow,
+            ],
+            tileMode: TileMode.mirror,
+          ),
+        ),
+      ),
+    );
   }
 }
