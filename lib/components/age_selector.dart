d24eac97ff57c626b0daac6a5d952415e12f3f8d
Vaishali Vyas <sayvilahsiav@outlook.com>
2024-07-04 09:46:47 +0530

    Changed Age Selector to a scrollable list from a slider.

--- lib/components/age_selector.dart
+++ lib/components/age_selector.dart
@@ -13,75 +13,107 @@ class AgeSelector extends StatefulWidget {
 
 class _AgeSelectorState extends State<AgeSelector> {
   late int age;
+  ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
     age = widget.initialAge;
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      _scrollToAge(age);
+    });
+  }
+
+  void _scrollToAge(int age) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double centerOffset = (screenWidth - itemWidth) / 2;
+    final double position = (age - 10) * itemWidth - centerOffset;
+    _scrollController.animateTo(
+      position,
+      duration: Duration(seconds: 1),
+      curve: Curves.easeInOut,
+    );
   }
 
   void _incrementAge() {
-    setState(() {
-      age++;
-    });
-    widget.onAgeChanged(age);
+    if (age < 100) {
+      setState(() {
+        age++;
+      });
+      _scrollToAge(age);
+      widget.onAgeChanged(age);
+    }
   }
 
   void _decrementAge() {
-    setState(() {
-      age--;
-    });
-    widget.onAgeChanged(age);
+    if (age > 10) {
+      setState(() {
+        age--;
+      });
+      _scrollToAge(age);
+      widget.onAgeChanged(age);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 324,
-      height: 64,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16.0),
-        boxShadow: [
-          // Drop shadow
-          BoxShadow(
-            color: kColorDarkText,
-            blurRadius: 4,
-            spreadRadius: 0.25,
-            offset: Offset(0, 0.25),
+    return Column(
+      children: [
+        Container(
+          height: itemWidth,
+          child: Row(
+            children: [
+              IconButton(
+                icon: Icon(Icons.arrow_left, size: 32, color: kColorDarkText),
+                onPressed: _decrementAge,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  controller: _scrollController,
+                  itemCount: 91, // Age range from 10 to 100
+                  itemBuilder: (context, index) {
+                    final displayAge = index + 10;
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          age = displayAge;
+                        });
+                        widget.onAgeChanged(age);
+                      },
+                      child: Container(
+                        width: itemWidth,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: age == displayAge ? kInnerShadow : kDropShadow,
+                          border: Border.all(
+                            color: age == displayAge ? kColorDarkText : kColorDarkGreen,
+                            width: 1,
+                          ),
+                        ),
+                        margin: EdgeInsets.symmetric(horizontal: 4),
+                        child: Text(
+                          displayAge.toString(),
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: age == displayAge ? kColorLightText : kColorDarkGreen,
+                            fontWeight: age == displayAge ? FontWeight.bold : FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.arrow_right, size: 32, color: kColorDarkText),
+                onPressed: _incrementAge,
+              ),
+            ],
           ),
-          BoxShadow(color: kColorLightText),
-          // Inner shadow
-          BoxShadow(color: kColorLightGreen),
-          BoxShadow(
-            color: kColorLightText,
-            blurRadius: 25,
-            spreadRadius: -0.25,
-            offset: Offset(0, 1),
-          ),
-        ],
-        border: Border.all(
-          color: kColorDarkGreen,
-          width: 1,
         ),
-      ),
-      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          IconButton(
-            icon: Icon(Icons.arrow_left, size: 32, color: kColorDarkText),
-            onPressed: _decrementAge,
-          ),
-          Text(
-            age.toString(),
-            style: Theme.of(context).textTheme.displayMedium,
-          ),
-          IconButton(
-            icon: Icon(Icons.arrow_right, size: 32, color: kColorDarkText),
-            onPressed: _incrementAge,
-          ),
-        ],
-      ),
+      ],
     );
   }
 }
